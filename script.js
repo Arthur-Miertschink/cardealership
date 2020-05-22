b802c1d8c2161e7ace0543aa4207fadeb0269d16
Arthur <arthursm50@gmail.com>
2020-05-22 10:46:27 -0300

    Criando esqueleto da página sobre

---
+++ script.js
@@ -0,0 +1,9 @@
+// Função para mudança de páginas no site
+
+function sobre(){
+    window.location.href = "file:///C:/Users/arthu/Desktop/Site%20loja%20de%20carros%20-%20Trabalho%20WEB//sobre.html";
+}
+
+function home(){
+    window.location.href = "file:///C:/Users/arthu/Desktop/Site%20loja%20de%20carros%20-%20Trabalho%20WEB//index.html";
+}
