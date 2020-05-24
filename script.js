aede8aa146697766fc4609bce606d660d25a498b
Arthur <arthursm50@gmail.com>
2020-05-24 12:32:36 -0300

    Inerindo conteúdo na página de contato

--- script.js
+++ script.js
@@ -7,3 +7,7 @@ function sobre(){
 function home(){
     window.location.href = "file:///C:/Users/arthu/Desktop/Site%20loja%20de%20carros%20-%20Trabalho%20WEB//index.html";
 }
+
+function contato(){
+    window.location.href = "file:///C:/Users/arthu/Desktop/Site%20loja%20de%20carros%20-%20Trabalho%20WEB//contato.html";
+}
