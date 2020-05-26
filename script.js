4ffb4e2555425053e21216c84c433057caf1d8f4
Arthur <arthursm50@gmail.com>
2020-05-26 13:58:16 -0300

    Detalhamento e ajustes de páginas

--- script.js
+++ script.js
@@ -1,13 +1,15 @@
 // Função para mudança de páginas no site
 
 function sobre(){
-    window.location.href = "file:///C:/Users/arthu/Desktop/Site%20loja%20de%20carros%20-%20Trabalho%20WEB//sobre.html";
+    window.location.href = "https://localhost/cardealership/sobre.html";
 }
 
 function home(){
-    window.location.href = "file:///C:/Users/arthu/Desktop/Site%20loja%20de%20carros%20-%20Trabalho%20WEB//index.html";
+    window.location.href = "https://localhost/cardealership/index.html";
 }
 
 function contato(){
-    window.location.href = "file:///C:/Users/arthu/Desktop/Site%20loja%20de%20carros%20-%20Trabalho%20WEB//contato.html";
+    window.location.href = "https://localhost/cardealership/contato.html";
 }
+
+// Função para mudar a cor do botão do menu enquanto ele estiver selecionado
