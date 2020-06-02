c836dcf7eb5a7a07370854dc819b975d8a12daee
Arthur <arthursm50@gmail.com>
2020-06-02 18:11:33 -0300

    Fazendo página do carro

--- script.js
+++ script.js
@@ -16,4 +16,37 @@ function estoque(){
     window.location.href = "https://localhost/cardealership/estoque.html";
 }
 
+function carro(){
+  window.location.href = "https://localhost/cardealership/carros/civic_touring_2017.html"
+}
 
+// funcao para fazer a galeria funcionar
+
+var slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+  ga('send', 'event', 'galeria', 'next_prev', 'Titulo da página');
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  if (n > slides.length) {
+    slideIndex = 1
+  }
+  if (n < 1) {
+    slideIndex = slides.length
+  }
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slides[slideIndex - 1].style.display = "block";
+}
